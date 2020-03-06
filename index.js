3a7dcb99dca3833adfa64f2f9c6a2fb35996d57f
imam932 <nawawi932@gmail.com>
2020-03-06 15:22:33 +0700

    add endpoind get provinces and cities

---
+++ index.js
@@ -0,0 +1,50 @@
+// server
+const express = require('express')
+const {init} = require('rajaongkir-node-js')
+const request = init('0df6d5bf733214af6c6644eb8717c92c', 'starter')
+
+const app = express()
+
+app.get('/provinces', async (req, res) => {
+    try {
+        const url = '/province'
+		const regionType = request.get(url)
+		regionType.then(x => {
+				res.write(x)
+				res.end()
+		})
+    } catch (error) {
+        console.log(error);
+    }
+  });
+
+app.get('/cities', async (req, res) => {
+    try {
+        const url = '/city'
+		const regionType = request.get(url)
+		regionType.then(x => {
+				res.write(x)
+				res.end()
+		})
+    } catch (error) {
+        console.log(error);
+    }
+  });
+  
+app.get('/cities', async (req, res) => {
+    try {
+        const url = '/city'
+		const regionType = request.get(url)
+		regionType.then(x => {
+				res.write(x)
+				res.end()
+		})
+    } catch (error) {
+        console.log(error);
+    }
+  });
+
+
+app.listen(8000, () => {
+console.log('Server is up on port 8000');
+});
