7c74f98c1de06f2f2df764f1a1b55c43b400adaf
imam932 <nawawi932@gmail.com>
2020-03-06 22:27:24 +0700

    fix resolveConfig

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
+require('dotenv').config()
 
-// try {
-//     let fileRead = fs.readFileSync('./service.yml', 'utf8')
-//     let data = yaml.safeLoad(fileRead)
-//     let hasil = JSON.parse(data) 
-//     console.log(hasil);
-    
-// } catch (error) {
-//     console.log(error);
-    
-// }
-function loadConfig(file){
+function resolveConfig(file){
+  const sql_client = process.env.SQL_CLIENT
+  const sql_host = process.env.SQL_HOST
+  const sql_port = process.env.SQL_PORT
+  const sql_user = process.env.SQL_USER
+  const sql_password = process.env.SQL_PASSWORD
+  const sql_database = process.env.SQL_DATABASE
+
     return new Promise(function(resolve, reject) {
       fs.readFile('./'+file+'.yml', 'utf8', function (err, data){
         if (err) reject(err);
         let datas = yaml.safeLoad(data)
-  
-        // var config = JSON.parse(datas);
-        // resolve(config); 
-        console.log(datas);
+
+        var config = JSON.stringify(datas, null, 4);
+        resolve(config); 
+        // console.log(config);
+        
+        let format_data = {
+          storages: {
+            database: {
+              client: sql_client,
+              connection: {
+                host: sql_host,
+                port: sql_port,
+                user: sql_user,
+                password: sql_password,
+                database: sql_database,
+              }
+            }
+          }
+        }
+      
+        fs.writeFileSync(file+'.yml', yaml.safeDump(format_data), 'utf8', err => {
+          if (err) console.log(err)
+        })
         
       });
     })
   };
 
-  loadConfig('service').then(function(config) {
-    // do something with the config
-    console.log(config);
-    
-  }).catch(function(err){
-      console.log(err);
-      
-    // do something with the error
-  });
+ resolveConfig('service')
+ .then(function(result) {
+  console.log(result); // "initResolve"
+  return result;
+})
+.then(function(result) {
+  console.log(result); // "normalReturn"
+});
